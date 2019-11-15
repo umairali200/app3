53a354a962979676e7d721a1753fdb62ea797717
umairali20 <51050652+umairali20@users.noreply.github.com>
2019-11-15 18:01:32 +0500

    Updates

---
+++ precache-manifest.3f798c911e20d08a83bcbbfe4dac0fee.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8b12caa87ad52677b705616e9ed09b",
+    "url": "/umairali200/app3.git/index.html"
+  },
+  {
+    "revision": "6a329da16ff66123c5eb",
+    "url": "/umairali200/app3.git/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "91a2bbd9bc47a858382b",
+    "url": "/umairali200/app3.git/static/css/main.fc941960.chunk.css"
+  },
+  {
+    "revision": "6a329da16ff66123c5eb",
+    "url": "/umairali200/app3.git/static/js/2.d52abac3.chunk.js"
+  },
+  {
+    "revision": "91a2bbd9bc47a858382b",
+    "url": "/umairali200/app3.git/static/js/main.b452ee25.chunk.js"
+  },
+  {
+    "revision": "d2d86211f36a5ce6fbf0",
+    "url": "/umairali200/app3.git/static/js/runtime-main.3d663844.js"
+  },
+  {
+    "revision": "475df43e28646501275986cae86490ed",
+    "url": "/umairali200/app3.git/static/media/Eye.475df43e.jpg"
+  },
+  {
+    "revision": "3dd6bca48f9d537b52131d73edd5562b",
+    "url": "/umairali200/app3.git/static/media/Flow.3dd6bca4.jpeg"
+  },
+  {
+    "revision": "93f40e10055dcf8db1910341d961de22",
+    "url": "/umairali200/app3.git/static/media/N1.93f40e10.jpg"
+  },
+  {
+    "revision": "943ba879dfd376e4f02515c222ac3c1b",
+    "url": "/umairali200/app3.git/static/media/N2.943ba879.jpg"
+  },
+  {
+    "revision": "b0b6585e2792f687810b784637638d23",
+    "url": "/umairali200/app3.git/static/media/N3.b0b6585e.jpg"
+  },
+  {
+    "revision": "5083d52c41babdb0b31cb34e2f150368",
+    "url": "/umairali200/app3.git/static/media/N4.5083d52c.jpg"
+  },
+  {
+    "revision": "bdcb700d5334594698cc70c8746a16fd",
+    "url": "/umairali200/app3.git/static/media/N5.bdcb700d.jpg"
+  },
+  {
+    "revision": "db7315b786b7d8268d91410dd54623ee",
+    "url": "/umairali200/app3.git/static/media/N6.db7315b7.jpg"
+  },
+  {
+    "revision": "57d33976c34cd3598e40af80c303f613",
+    "url": "/umairali200/app3.git/static/media/N7.57d33976.jpg"
+  },
+  {
+    "revision": "1e28552d78c8b1dc474d9378471820bf",
+    "url": "/umairali200/app3.git/static/media/Nail.1e28552d.jpg"
+  },
+  {
+    "revision": "5ce46dc662cdb2c4c455280e5c737a8c",
+    "url": "/umairali200/app3.git/static/media/Shade.5ce46dc6.jpg"
+  }
+]);
